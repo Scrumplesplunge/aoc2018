df01e64cf036021531a4df4cf92ab7635188de6d
Joe Fowler <scrumplesplunge@gmail.com>
2018-12-05 08:50:55 +0000

    Changed algorithm for day 1 to get 3x speed.

--- src/day1.cc
+++ src/day1.cc
@@ -3,6 +3,7 @@
 #include "puzzles.h"
 
 #include <iterator>
+#include <iostream>
 #include <numeric>
 #include <unordered_set>
 #include <sstream>
@@ -18,10 +19,17 @@ int Solve1B() {
   std::istringstream input{std::string{kPuzzle1}};
   const std::vector<int> deltas{std::istream_iterator<int>{input}, {}};
   std::unordered_set<int> seen;
+  // Try to find a match in the first iteration.
   int frequency = 0;
+  for (int x : deltas) {
+    if (!seen.insert(frequency).second) return frequency;
+    frequency += x;
+  }
+  // Since this is a periodic thing, the first conflict must happen in the ones
+  // that have already been seen.
   while (true) {
     for (int x : deltas) {
-      if (!seen.insert(frequency).second) return frequency;
+      if (seen.count(frequency)) return frequency;
       frequency += x;
     }
   }
