df5a8ffd0adafb6a8381a54451bc726752097755
Joe Fowler <scrumplesplunge@gmail.com>
2018-12-13 21:35:11 +0000

    Simplified day 13.

--- src/day13.cc
+++ src/day13.cc
@@ -14,12 +14,16 @@ constexpr int kGridWidth = 150;
 constexpr int kGridHeight = 150;
 using Grid = std::array<std::array<char, kGridWidth>, kGridHeight>;
 
-enum class Direction : unsigned char {
-  kUp,
-  kRight,
-  kDown,
-  kLeft,
-};
+enum class Direction : unsigned char { kUp, kRight, kDown, kLeft };
+enum class Choice : unsigned char { kLeft, kStraight, kRight };
+struct Position { unsigned char x, y; };
+struct Cart { Position position; Direction direction; Choice next_choice; };
+struct Input { Grid grid; std::vector<Cart> carts; };
+
+// We reserve one cart for representing crashes. This is outside of the range
+// where normal carts would be.
+constexpr Cart kCrashedCart{
+    {kGridWidth, kGridHeight}, Direction::kUp, Choice::kStraight};
 
 // Incrementing or decrementing a direction is rotating it clockwise or
 // counterclockwise respectively.
@@ -35,12 +39,6 @@ constexpr Direction operator++(Direction& value, int) {
   return old;
 }
 
-enum class Choice : unsigned char {
-  kLeft,
-  kStraight,
-  kRight,
-};
-
 // Incrementing a choice cycles through the options in the order they are
 // supposed to occur.
 constexpr Choice operator++(Choice& value, int) {
@@ -49,55 +47,33 @@ constexpr Choice operator++(Choice& value, int) {
   return old;
 }
 
-struct Position { unsigned char x, y; };
-
-// Order positions such that top left < top right < bottom left < bottom right.
-constexpr bool operator<(Position a, Position b) {
-  return std::tie(a.y, a.x) < std::tie(b.y, b.x);
-}
-
 constexpr bool operator==(Position a, Position b) {
   return a.x == b.x && a.y == b.y;
 }
 
-struct Cart {
-  Position position;
-  Direction direction;
-  Choice next_choice;
-};
-
-// We reserve one cart for representing crashes. This is outside of the range
-// where normal carts would be.
-constexpr Cart kCrashedCart{
-    {kGridWidth, kGridHeight}, Direction::kUp, Choice::kStraight};
-
-// Order carts by their position.
-constexpr bool operator<(Cart a, Cart b) { return a.position < b.position; }
+// Order carts such that top left < top right < bottom left < bottom right.
+constexpr bool operator<(Cart a, Cart b) {
+  return std::tie(a.position.y, a.position.x) <
+         std::tie(b.position.y, b.position.x);
+}
 
 constexpr bool operator==(Cart a, Cart b) {
   return a.position == b.position && a.direction == b.direction &&
          a.next_choice == b.next_choice;
 }
 
-struct Input {
-  Grid grid;
-  std::vector<Cart> carts;
-};
-
 Input GetInput() {
 #ifndef NDEBUG
   // Verify that the input is a 150x150 grid.
   assert(kPuzzle13.length() == (kGridWidth + 1) * kGridHeight);
-  for (int i = 0; i < kGridHeight; i++) {
+  for (int i = 0; i < kGridHeight; i++)
     assert(kPuzzle13[(1 + kGridWidth) * (i + 1) - 1] == '\n');
-  }
 #endif  // NDEBUG
   Input input;
   for (unsigned char y = 0; y < kGridHeight; y++) {
     int row_offset = (1 + kGridWidth) * y;
     for (unsigned char x = 0; x < kGridWidth; x++) {
-      int offset = row_offset + x;
-      char cell = kPuzzle13[offset];
+      char cell = kPuzzle13[row_offset + x];
       switch (cell) {
         case '^':
           input.carts.push_back(Cart{{x, y}, Direction::kUp, Choice::kLeft});
@@ -128,14 +104,26 @@ constexpr auto At(Position position) {
 }
 
 constexpr Cart AdvanceCart(const Grid& grid, Cart cart) {
-  assert(1 <= cart.position.x && cart.position.x < kGridWidth - 1);
-  assert(1 <= cart.position.y && cart.position.y < kGridHeight - 1);
+  // Find the new position.
   switch (cart.direction) {
-    case Direction::kUp: cart.position.y--; break;
-    case Direction::kDown: cart.position.y++; break;
-    case Direction::kLeft: cart.position.x--; break;
-    case Direction::kRight: cart.position.x++; break;
+    case Direction::kUp:
+      assert(0 < cart.position.y);
+      cart.position.y--;
+      break;
+    case Direction::kDown:
+      assert(cart.position.y < kGridWidth - 1);
+      cart.position.y++;
+      break;
+    case Direction::kLeft:
+      assert(0 < cart.position.x);
+      cart.position.x--;
+      break;
+    case Direction::kRight:
+      assert(cart.position.x < kGridWidth - 1);
+      cart.position.x++;
+      break;
   }
+  // Find the new orientation.
   switch (grid[cart.position.y][cart.position.x]) {
     case '\\':
       switch (cart.direction) {
@@ -163,36 +151,6 @@ constexpr Cart AdvanceCart(const Grid& grid, Cart cart) {
   }
   return cart;
 }
-
-// void ShowCart(const Grid& grid, Cart cart) {
-//   constexpr int kContext = 3;
-//   int min_x = cart.position.x >= kContext ? cart.position.x - kContext : 0;
-//   int max_x = cart.position.x < kGridWidth - kContext
-//                   ? cart.position.x + kContext
-//                   : kGridWidth - 1;
-//   int min_y = cart.position.y >= kContext ? cart.position.y - kContext : 0;
-//   int max_y = cart.position.y < kGridHeight - kContext
-//                   ? cart.position.y + kContext
-//                   : kGridHeight - 1;
-//   for (int y = min_y; y <= max_y; y++) {
-//     for (int x = min_x; x <= max_x; x++) {
-//       if (x == cart.position.x && y == cart.position.y) {
-//         std::cout << "\x1b[32;1m";
-//         switch (cart.direction) {
-//           case Direction::kUp: std::cout << '^'; break;
-//           case Direction::kRight: std::cout << '>'; break;
-//           case Direction::kDown: std::cout << 'v'; break;
-//           case Direction::kLeft: std::cout << '<'; break;
-//           default: std::cout << '?'; break;
-//         }
-//         std::cout << "\x1b[0m";
-//       } else {
-//         std::cout << grid[y][x];
-//       }
-//     }
-//     std::cout << '\n';
-//   }
-// }
 
 Position RunUntilCollision(const Grid& grid, std::vector<Cart> carts) {
   while (true) {
